94c62050787d1f2fe319fc2cb09a6a35d6f8f64a
Avaerian <55154263+Avaerian@users.noreply.github.com>
2020-11-05 08:12:19 -0500

    Reevaluated and reorganized methods for serialization and deserialization

--- src/main/java/org/avaeriandev/titancore/TitanPlayer.java
+++ src/main/java/org/avaeriandev/titancore/TitanPlayer.java
@@ -11,63 +11,32 @@ import java.util.Map;
 
 public class TitanPlayer {
 
-    private Player plr;
+    private OfflinePlayer plr;
     private int tickets;
     private Map<Integer, Long> activatedQuests;
     private Map<Integer, Long> completedQuests;
-
-    public static TitanPlayer load(Player plr) {
-
-        // Check if player is already loaded
-        if(TitanAPI.playerData.containsKey(plr)) {
-            return TitanAPI.playerData.get(plr);
-        }
-
-        // Check if player has save file
-        File saveFile = new File(TitanPlugin.plrDataDir, plr.getUniqueId().toString() + ".yml");
-        if(saveFile.exists()) {
-            FileConfiguration config = YamlConfiguration.loadConfiguration(saveFile);
-
-            int tickets = config.getInt("tickets");
-            Map<Integer, Long> activatedQuests = (Map<Integer, Long>) config.get("activated-quests");
-            Map<Integer, Long> completedQuests = (Map<Integer, Long>) config.get("completed-quests");
-
-            TitanPlayer titanPlayer = new TitanPlayer(plr, tickets, activatedQuests, completedQuests);
-            TitanAPI.playerData.put(plr, titanPlayer);
-            return titanPlayer;
-        }
-
-        // Load empty data for player
-        TitanPlayer titanPlayer = new TitanPlayer(plr,0, new HashMap<Integer, Long>(), new HashMap<Integer, Long>());
-        TitanAPI.playerData.put(plr, titanPlayer);
-        return titanPlayer;
+    
+    public Map<String, Object> serialize() {
+        
+        Map<String, Object> serialMap = new HashMap<>();
+        
+        serialMap.put("uuid", plr.getUniqueID().toString());
+        serialMap.put("tickets", tickets);
+        serialMap.put("activated-quests", activatedQuests);
+        serialMap.put("completed-quests", completedQuests);
+        
+        return serialMap;
     }
-
-    private TitanPlayer(Player plr, int tickets, Map<Integer, Long> activatedQuests, Map<Integer, Long> completedQuests) {
-        // TODO: move file loader to from static load() to constructor
-        this.tickets = tickets;
-        this.activatedQuests = activatedQuests;
-        this.completedQuests = completedQuests;
+    
+    public TitanPlayer(Map<String, Object> serialMap) {
+        this.plr = Bukkit.getOfflinePlayer(UUID.fromString((String) serialMap.get("uuid")));
+        this.tickets = (int) serialMap.get("tickets");
+        this.activatedQuests = (Map<Integer, Long>) serialMap.get("activated-quests");
+        this.completedQuests = (Map<Integer, Long>) serialMap.get("completed-quests");
     }
-
-    public void save() {
-        try {
-            File saveFile = new File(TitanPlugin.plrDataDir, plr.getUniqueId().toString() + ".yml");
-            FileConfiguration config = YamlConfiguration.loadConfiguration(saveFile);
-
-            config.set("tickets", tickets);
-            config.set("activated-quests", activatedQuests);
-            config.set("completed-quests", completedQuests);
-
-            config.save(saveFile);
-        } catch(IOException e) {
-            System.err.println("There was a fatal error saving player data!");
-            e.printStackTrace();
-        }
-    }
-
-    public void unregister() {
-        TitanAPI.playerData.remove(plr);
+    
+    public OfflinePlayer getPlayer() {
+        return plr;
     }
 
     public int getTickets() {
