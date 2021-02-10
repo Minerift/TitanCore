3d3335e18f524de2eda61d730a991a41af145306
Avaerian <55154263+Avaerian@users.noreply.github.com>
2021-02-10 17:21:06 -0500

    Remove Christmas content

--- src/main/java/org/avaeriandev/titancore/christmas/giftrewards/GiftReward.java
+++
@@ -1,9 +0,0 @@
-package org.avaeriandev.titancore.christmas.giftrewards;
-
-import org.bukkit.entity.Player;
-
-public abstract class GiftReward {
-
-    public abstract void rewardPlayer(Player plr);
-
-}
