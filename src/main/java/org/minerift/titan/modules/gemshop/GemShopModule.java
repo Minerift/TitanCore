855bcc2e263527475688f1ee5d82c63349659d98
Avaerian <55154263+Avaerian@users.noreply.github.com>
2021-05-04 00:20:22 -0400

    Complete data IO for module, add scheduler for refreshing gem shop daily

--- src/main/java/org/minerift/titan/modules/gemshop/GemShopModule.java
+++ src/main/java/org/minerift/titan/modules/gemshop/GemShopModule.java
@@ -2,16 +2,24 @@ package org.minerift.titan.modules.gemshop;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
+import org.minerift.titan.TitanPlugin;
+import org.minerift.titan.TitanProfile;
 import org.minerift.titan.modules.Module;
+import org.minerift.titan.modules.gemshop.commands.GemShopCommand;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 public class GemShopModule extends Module {
 
+    private static final int TICK = 20;
     private static final int SLOT_COUNT = 3;
-    private static final File MODULE_FILE = null;
+    private static final int MAX_SLOT_PURCHASES = 3;
     private static final ObjectMapper MAPPER = new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
 
     private BukkitTask timestampChecker;
@@ -20,14 +28,35 @@ public class GemShopModule extends Module {
     @Override
     protected void onEnable() {
 
+        registerCommand("gemshop", new GemShopCommand());
+
         // Deserialize file (if applicable)
         try {
-            this.data = MODULE_FILE.length() != 0 ? MAPPER.readValue(MODULE_FILE, GemShopData.class) : new GemShopData();
+            File moduleFile = getModuleFile();
+            this.data = moduleFile.length() != 0 ? MAPPER.readValue(moduleFile, GemShopData.class) : new GemShopData();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // Prepare expiration scheduler
+        this.timestampChecker = new BukkitRunnable() {
+            @Override
+            public void run() {
+                boolean hasExpired = System.currentTimeMillis() > getExpirationTimestamp();
+                if(hasExpired) {
 
+                    // Update module
+                    data.setExpireTimestamp(data.nextExpireTimestamp());
+                    data.setSlots(data.selectNewOffers());
+                    data.setVersion(data.getVersion() + 1);
+
+                    // Update player data
+                    TitanProfile.list().forEach(profile -> profile.updateGemShopData());
+
+                    System.out.println("Gem shop updated!");
+                }
+            }
+        }.runTaskTimer(TitanPlugin.getInstance(), 0, 10 * TICK);
     }
 
     @Override
@@ -38,7 +67,8 @@ public class GemShopModule extends Module {
 
         // Serialize data
         try {
-            MAPPER.writeValue(MODULE_FILE, data);
+            File moduleFile = getModuleFile();
+            MAPPER.writeValue(moduleFile, data);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,5 +89,17 @@ public class GemShopModule extends Module {
 
     public int getSlotCount() {
         return SLOT_COUNT;
+    }
+
+    public int getMaxSlotPurchases() {
+        return MAX_SLOT_PURCHASES;
+    }
+
+    private File getModuleFile() throws IOException {
+        File file = new File(TitanPlugin.getInstance().getDataFolder(), "gemshop.json");
+        if(!file.exists()) {
+                file.createNewFile();
+        }
+        return file;
     }
 }
