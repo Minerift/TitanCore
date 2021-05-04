835bb07f46ba762090018f3007e739e7bcaf97fc
Avaerian <55154263+Avaerian@users.noreply.github.com>
2021-05-04 00:25:29 -0400

    Add check for version consistency across server and client on join

--- src/main/java/org/minerift/titan/listeners/PlayerJoinListener.java
+++ src/main/java/org/minerift/titan/listeners/PlayerJoinListener.java
@@ -1,5 +1,7 @@
 package org.minerift.titan.listeners;
 
+import org.avaeriandev.api.util.BaseUtils;
+import org.minerift.titan.TitanPlugin;
 import org.minerift.titan.TitanProfile;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -7,15 +9,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.minerift.titan.modules.gemshop.GemShopModule;
 
 public class PlayerJoinListener implements Listener {
+
+    private final GemShopModule GEM_SHOP_MODULE = TitanPlugin.getModule(GemShopModule.class);
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
 
         // Load player data
         Player plr = e.getPlayer();
-        TitanProfile.load(plr);
+        TitanProfile profile = TitanProfile.load(plr);
+
+        if(profile.getLocalGemShopVersion() != GEM_SHOP_MODULE.getVersion()) {
+            profile.updateGemShopData();
+            plr.sendMessage(BaseUtils.chat("&aWhile you were offline, the gem shop has restocked!"));
+        }
 
         // Give player first-join bread (if eligible)
         if(!plr.hasPlayedBefore()) {
