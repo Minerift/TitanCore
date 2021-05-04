1b3d7f8a4f82f0c6769719edcffb7c2e597562f2
Avaerian <55154263+Avaerian@users.noreply.github.com>
2021-05-04 00:23:56 -0400

    Display offers in panel

--- src/main/java/org/minerift/titan/modules/gemshop/panel/GemShopPanel.java
+++ src/main/java/org/minerift/titan/modules/gemshop/panel/GemShopPanel.java
@@ -19,7 +19,7 @@ public class GemShopPanel extends Panel {
     private static final String TITLE = "&8Gem Shop";
     private static final int ROWS = 3;
 
-    private static final int[] OFFER_SLOTS = {1, 2, 3};
+    private static final int[] OFFER_SLOTS = {11, 14, 17};
     private static final ItemStack ERROR_ICON = new ItemBuilder(new ItemStack(Material.BARRIER))
             .setName("&cYou can't purchase more of this item!")
             .create();
@@ -36,10 +36,15 @@ public class GemShopPanel extends Panel {
 
         // Filler item
         for(int i = 0; i < getPanel().getSize(); i++) {
-            layout.put(i, new PanelIcon(new ItemStack(Material.STAINED_GLASS_PANE), null));
+            layout.put(i + 1, new PanelIcon(new ItemStack(Material.STAINED_GLASS_PANE), null));
         }
 
         // Add offers to panel
+        for(int i = 0; i < OFFER_SLOTS.length; i++) {
+            int slot = OFFER_SLOTS[i];
+            layout.put(slot, new PanelIcon(profile.canPurchase(i) ? MODULE.getOffers()[i].getIcon() : new ItemStack(ERROR_ICON), null));
+        }
 
+        super.loadLayout(layout);
     }
 }
