cd23d6a47efce5d4c2674c68eebde845a53a32a4
Avaerian <55154263+Avaerian@users.noreply.github.com>
2021-05-04 00:26:16 -0400

    Add helper methods

--- src/main/java/org/minerift/titan/modules/gemshop/GemShopData.java
+++ src/main/java/org/minerift/titan/modules/gemshop/GemShopData.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.minerift.titan.TitanPlugin;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class GemShopData {
 
@@ -11,8 +15,8 @@ public class GemShopData {
     private GemShopModule MODULE = TitanPlugin.getModule(GemShopModule.class);
 
     private int version = 0;
-    private GemShopOffer[] slots = new GemShopOffer[MODULE.getSlotCount()];
-    private long expireTimestamp;
+    private GemShopOffer[] slots = selectNewOffers();
+    private long expireTimestamp = nextExpireTimestamp();
 
     GemShopData() {}
 
@@ -38,5 +42,21 @@ public class GemShopData {
 
     public void setExpireTimestamp(long expireTimestamp) {
         this.expireTimestamp = expireTimestamp;
+    }
+
+    public long nextExpireTimestamp() {
+        Calendar newExpireTimestamp = Calendar.getInstance();
+        newExpireTimestamp.setTime(new Date());
+        newExpireTimestamp.add(Calendar.HOUR_OF_DAY, 24);
+
+       return newExpireTimestamp.getTimeInMillis();
+    }
+
+    public GemShopOffer[] selectNewOffers() {
+        GemShopOffer[] newOffers = new GemShopOffer[MODULE.getSlotCount()];
+        for(int i = 0; i < newOffers.length; i++) {
+            newOffers[i] = GemShopOffer.random();
+        }
+        return newOffers;
     }
 }
